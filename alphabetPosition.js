f1ea5fd463f34600afcc257a1721c48f7c866512
Hello! I'm Kyalo <72204484+RemmyKyalo@users.noreply.github.com>
2023-08-11 15:44:44 +0300

    Create alphabetPosition.js

---
+++ alphabetPosition.js
@@ -0,0 +1,27 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  const alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const textArray = text.toLowerCase().split(""); 
+
+  const charIndex = [];
+  for (let i = 0; i < textArray.length; i++) {
+    if (alphabetArray.includes(textArray[i])) {
+      charIndex.push(alphabetArray.indexOf(textArray[i]) + 1);
+    }
+  }
+
+  return charIndex.join(" ");
+}
+
+
