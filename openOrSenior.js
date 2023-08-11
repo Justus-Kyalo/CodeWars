0e9263005304ea88f12aab033f6db9d8eef02710
Hello! I'm Kyalo <72204484+RemmyKyalo@users.noreply.github.com>
2023-08-11 13:56:10 +0300

    Update openOrSenior.js

--- openOrSenior.js
+++ openOrSenior.js
@@ -11,3 +11,30 @@
 // Example
 // input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data){
+  // ...
+  let array = [];
+  for( i = 0; i<data.length; i++){
+     if(data[i][0]>=55 && data[i][1]>7){
+       array.push("Senior");
+    }
+  else{
+     array.push("Open");
+  }
+  }
+   
+  return array ;
+}
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Fixed tests", () => {
+//     assert.deepEqual(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior'])
+//     assert.deepEqual(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]),['Open', 'Open', 'Open', 'Open'])
+//     assert.deepEqual(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]]),['Senior', 'Open', 'Open', 'Open'])
+//   });
+// });
