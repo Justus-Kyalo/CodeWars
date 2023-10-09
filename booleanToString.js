a4e4975204cd651c748f3005feac481620810f04
RemmyKyalo <chaloremmy@gmail.com>
2023-10-09 16:45:20 +0300

    test

--- booleanToString.js
+++ booleanToString.js
@@ -2,17 +2,16 @@
 
 // Note: Only valid inputs will be given.
 
+function booleanToString(b) {
+  //your code here
+  return b ? "true" : "false";
+}
 
-// function booleanToString(b){
-//   //your code here
-//   return b ? "true" : "false" ;
-// }
+// const Test = require('@codewars/test-compat');
 
-const Test = require('@codewars/test-compat');
-
-describe("Tests", () => {
-  it("test", () => {
-Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
-Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
-  });
-});
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+// Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+//   });
+// });
