66070574e9c3f3e1b7e8ce14326611daaaa8ecad
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-01-15 00:04:14 +0300

    Update gradingStudents.js

--- gradingStudents.js
+++ gradingStudents.js
@@ -50,4 +50,27 @@
 // Student  received a , and the next multiple of  from  is . Since , the student's grade is rounded to .
 // Student  received a , and the next multiple of  from  is . Since , the grade will not be modified and the student's final grade is .
 // Student  received a , and the next multiple of  from  is . Since , the student's grade will be rounded to .
-// Student  received a grade below , so the grade will not be modified and the student's final grade is .
+// Student  received a grade below , so the grade will not be modified and the student's final grade is 
+
+function gradingStudents(grades) {
+    // Write your code here
+    let finalResults=[]
+    const lastDigs=[3,4,8,9]
+     grades.forEach(grade=>{
+        
+        if(grade<38){
+            finalResults.push(grade);
+        }
+        else if (((grade>=38)&&(grade<98)) && (lastDigs.includes(grade%10))){
+            
+            finalResults.push(grade+(5-(grade%5)))
+        }
+        else if(((grade>=38)&&(grade<98)) && (!lastDigs.includes(grade%10))){
+            finalResults.push(grade)
+            
+        }
+    })
+    
+    return finalResults
+
+}
