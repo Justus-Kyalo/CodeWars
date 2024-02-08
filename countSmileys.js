0ad6e36a7a4cf8a6dc1966a69c9231ebcfa9a9ad
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-02-08 19:16:03 +0300

    Update countSmileys.js

--- countSmileys.js
+++ countSmileys.js
@@ -34,6 +34,9 @@ const validSmilesArr=[...threeComps,...twoComps];
  
 }
 
+
+
+
 // Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
 
 // Rules for a smiling face:
