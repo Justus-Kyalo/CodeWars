6c272ee40079631c5b825a1dc52d98e211d67b2e
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-02-08 19:15:04 +0300

    Update countSmileys.js

--- countSmileys.js
+++ countSmileys.js
@@ -16,3 +16,40 @@
 // countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 // Note
 // In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  if (!arr.length) return 0;
+  let count=0
+  const eyes =[":",";"];
+  const nose =["-","~"];
+  const mouth =[")","D"];
+  let threeComps=[]
+  let twoComps=[]
+
+  
+
+ 
+  for (let i = 0; i < eyes.length; i++) {
+    for (let j = 0; j < nose.length; j++) {
+      for (let k = 0; k < mouth.length; k++) {
+        threeComps.push(eyes[i] + nose[j] + mouth[k]);
+        twoComps.push(eyes[i] + mouth[k]);
+      }
+    }
+  }
+
+const validSmilesArr=[...threeComps,...twoComps];
+  
+  
+
+   [...new Set(validSmilesArr)].forEach(val => {
+   arr.forEach(item=>{
+     if(item===val)return count++
+   })
+  });
+  
+  return count;
+  
+ 
+}
