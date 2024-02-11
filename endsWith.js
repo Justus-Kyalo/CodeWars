d268bae3be39bd4ef92631e10d3aebadddc03d1f
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-02-11 22:10:52 +0300

    Update endsWith.js

--- endsWith.js
+++ endsWith.js
@@ -1,4 +1,7 @@
-
+function solution(str, ending){
+  // TODO: complete
+  return str.endsWith(ending)
+}
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 // Examples:
