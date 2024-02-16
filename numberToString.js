5595c7007668e1168cc86fb0b2817a5ff999ece6
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-02-16 19:04:05 +0300

    Update numberToString.js

--- numberToString.js
+++ numberToString.js
@@ -1,3 +1,7 @@
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString()
+}
 // We need a function that can transform a number (integer) into a string.
 
 // What ways of achieving this do you know?
