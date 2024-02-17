93939fdca7c6702efb9aaa3add0f85e516af5693
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-02-17 22:46:23 +0300

    Update duplicateEncode.js

--- duplicateEncode.js
+++ duplicateEncode.js
@@ -1,3 +1,18 @@
+function duplicateEncode(word){
+    // ...
+  word = word.toLowerCase();  
+    let result = "";
+    for (let i = 0; i < word.length; i++) {
+        let char = word.charAt(i);
+        if (word.split(char).length - 1 > 1) {
+            result += ")";
+        } else {
+            result += "(";
+        }
+    }
+    return result;
+}
+
 // The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
 // Examples
