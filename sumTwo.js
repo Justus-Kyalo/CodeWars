82dd4817d21102167b9428b3c048d204bbe93d5a
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-02-22 21:23:18 +0300

    Update sumTwo.js

--- sumTwo.js
+++ sumTwo.js
@@ -1,1 +1,11 @@
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let result = numbers[i] + numbers[j];
+      if (result === target) {
+        return [i, j];
+      }
+    }
+  }
+}
 
