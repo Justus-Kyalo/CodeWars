78a36ed379e88b5cff1a2b1893a2b8f7193ae73d
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-03-03 17:35:29 +0300

    Update getMiddle.js

--- getMiddle.js
+++ getMiddle.js
@@ -1,3 +1,18 @@
+function getMiddle(s)
+{
+  //Code goes here!
+  if(s.length%2===0){
+    let half =s.length/2;
+    
+    return s.slice(half-1,(half+1));
+    
+  }
+  else {
+    let halfEvened=(s.length-1)/2;
+    
+    return s.slice(halfEvened,halfEvened+1);
+  }
+}
 // You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 // #Examples:
