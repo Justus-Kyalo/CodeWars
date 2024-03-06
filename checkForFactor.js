3b6fc1c442b2f1918bdb9f561948b1e52c145723
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-03-06 21:47:57 +0300

    Update checkForFactor.js

--- checkForFactor.js
+++ checkForFactor.js
@@ -1,3 +1,7 @@
+function checkForFactor (base, factor) {
+  // code here
+  return base%factor===0 ? true :false; 
+}
 // This function should test if the factor is a factor of base.
 
 // Return true if it is a factor or false if it is not.
