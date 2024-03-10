bf2a9e09cb5394bf0f063bb6109a26c9981aed0a
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-03-10 21:56:59 +0300

    Update makeNegative.js

--- makeNegative.js
+++ makeNegative.js
@@ -1,3 +1,8 @@
+function makeNegative(num) {
+  // Code?
+  return num<0 ? num : -num;
+}
+
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
 // Examples
