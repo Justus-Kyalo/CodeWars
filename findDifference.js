95ba8f57392699440d4b9d3f52836c4cc4899208
Hello! I'm Kyalo <chaloremmy@gmail.com>
2024-04-05 23:59:00 +0300

    Update findDifference.js

--- findDifference.js
+++ findDifference.js
@@ -1,3 +1,11 @@
+function findDifference(a, b) {
+  //loading...
+  let cuboid1 = a.reduce((acc,curr)=>acc*=curr,1);
+  let cuboid2 = b.reduce((acc,curr)=>acc*=curr,1)
+  
+  return Math.abs(cuboid1-cuboid2)
+}
+
 // In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
 
 // For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
